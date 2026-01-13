cc6a7d6bca1797838d8b5a42c2d6d6e0ec1b348c
walid-ghaziy <156791166+walid-ghaziy@users.noreply.github.com>
2026-01-13 18:52:56 +0300

    a

--- src/cours/Cours.js
+++ src/cours/Cours.js
@@ -1,6 +1,8 @@
 import { useNavigate } from 'react-router-dom';
 import im from '../componet/572c31288bf28db1672258d493bfba8b.jpg'
 import './cours.css'
+import csh from './image/c#.png'
+import cpl from './image/c++.png'
 export default function Cours(){
 const path = useNavigate()
     return(<>
@@ -8,9 +10,19 @@ const path = useNavigate()
         <center><h1>Basic Cours IT</h1></center>
         
         <div id="Box-cours">
-            <img src={im} alt="" />
+            <img src={csh} alt="" />
             <div id='info-cours'>
             <h3>C#</h3><small>6 Lissen</small><br />
+            <button value={"c1"} onClick={(e)=>{
+                alert("لده مه كئ نيزيك")
+                // path(`View/${e.target.value}`)
+            }}>View Cors</button>   
+            </div>
+        </div>
+        <div id="Box-cours">
+            <img src={cpl} alt="" />
+            <div id='info-cours'>
+            <h2>C#</h2><span>6 Lissen</span><br />
             <button value={"c"} onClick={(e)=>{
                 alert("لده مه كئ نيزيك")
                 // path(`View/${e.target.value}`)
